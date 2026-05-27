3a4b502d25580889cbd0b683f3aa3520c7698a0f
username0106 <lixiliang7@gmail.com>
2026-05-26 22:48:31 -0700

    completed part 1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,14 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js')
+        .then(reg => console.log('Service worker registered successfully:', reg))
+        .catch(err => console.log('Service worker registration failed:', err));
+    });
+  }
 }
 
 /**
@@ -100,6 +108,31 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+
+
+   const stored = localStorage.getItem('recipes');
+  if (stored) {
+    return JSON.parse(stored);
+  }
+
+  const recipes = [];
+
+  return new Promise((resolve, reject) => {
+    RECIPE_URLS.forEach(async (url) => {
+      try {
+        const response = await fetch(url);
+        const recipe = await response.json();
+        recipes.push(recipe);
+        if (recipes.length === RECIPE_URLS.length) {
+          saveRecipesToStorage(recipes);
+          resolve(recipes);
+        }
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    });
+  });
 }
 
 /**
